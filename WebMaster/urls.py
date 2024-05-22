086b3b5e6aa73966287e7229c9598fd6a3a02800
Danielk10 <danielpdiamon@gmail.com>
2024-05-22 00:07:27 -0400

    Corrección de urls principales

--- WebMaster/urls.py
+++ WebMaster/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/', include('apps.inicio.urls')),
+    path('', include('apps.inicio.urls')),
 ]
