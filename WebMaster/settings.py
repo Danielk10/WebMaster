a0b9516a32648e185ca421be2f245612d166f54c
Danielk10 <danielpdiamon@gmail.com>
2024-06-07 21:07:42 -0400

    Añadiendo el archivo 📂 de requerimientos

--- WebMaster/settings.py
+++ WebMaster/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
     'apps.inicio',
     'apps.productos',
     'apps.pedidos',
