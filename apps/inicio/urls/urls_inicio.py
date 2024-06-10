4be062de4d891b8b789b1867618f7b038e90b5f7
Danielk10 <danielpdiamon@gmail.com>
2024-06-10 01:04:49 -0400

    update urls_inicio.py

--- apps/inicio/urls/urls_inicio.py
+++ apps/inicio/urls/urls_inicio.py
@@ -11,10 +11,10 @@ from apps.inicio.views.formulario_blog_view import FormularioContacto
 
 #Configurando rutas
 urlpatterns = [
-   path('index', InicioView.as_view(),name='index'),
+   path('', InicioView.as_view(),name='index'),
    path('contacto-inicio', ContactanosInicioView.as_view(),name='contacto-inicio'),
    path('suscripcion_inicio', SuscriptorInicioView.as_view(),name='suscripcion_inicio'),
-   path('',Inicio.as_view(), name = 'index_blog'),
+   path('index_blog',Inicio.as_view(), name = 'index_blog'),
    path('videojuegos/',Listado.as_view(),{'nombre_categoria':'Videojuegos'}, name = 'videojuegos'),
    path('generales/',Listado.as_view(),{'nombre_categoria':'General'}, name = 'generales'),
    path('formulario_contacto/', FormularioContacto.as_view(), name = 'formulario_contacto'),
