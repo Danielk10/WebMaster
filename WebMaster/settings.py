1443437bd51904488246e580f0724fd3ee0ccc23
Danielk10 <danielpdiamon@gmail.com>
2024-06-09 22:14:28 -0400

    instalando app en settings.py

--- WebMaster/settings.py
+++ WebMaster/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'import_export',
+    'ckeditor',
     'apps.inicio',
     'apps.productos',
     'apps.pedidos',
