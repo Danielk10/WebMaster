2c87123df9db9ceaf97597fcdf7c5b35befcc4cd
Danielk10 <danielpdiamon@gmail.com>
2024-06-12 13:18:27 -0400

    update urls.py

--- WebMaster/urls.py
+++ WebMaster/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.inicio.urls.inicio_urls')),
     path('pedidos/',include(('apps.pedidos.urls.urls','pedidos'))),
-    path('productos/', include('apps.productos.urls.urls_productos'))
+    path('productos/', include('apps.productos.urls.urls_blog'))
     path('ventas/', include('apps.ventas.urls.ventas_urls'))
     path('contactanos/', include('apps.contactanos.urls.contactanos_urls'))
 
