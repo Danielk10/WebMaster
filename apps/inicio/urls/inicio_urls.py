aa1bea2f6afebf0d52df401444e05eada083e0da
Danielk10 <danielpdiamon@gmail.com>
2024-06-12 13:37:28 -0400

    update

--- apps/inicio/urls/inicio_urls.py
+++ apps/inicio/urls/inicio_urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from apps.inicio.views.inicio_principal_views import InicioView
 
-
-#Configurando rutas
 urlpatterns = [
    path('', InicioView.as_view(),name='index'),
    
