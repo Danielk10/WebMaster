93784b0df14b0e4080a8898e3383d42c015b7fc3
Danielk10 <danielpdiamon@gmail.com>
2024-06-12 13:13:36 -0400

    Corrección del archivo 📂 de configuración

--- apps/contactanos/apps.py
+++ apps/contactanos/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ContactanosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'contactanos'
+    name = 'apps.contactanos'
